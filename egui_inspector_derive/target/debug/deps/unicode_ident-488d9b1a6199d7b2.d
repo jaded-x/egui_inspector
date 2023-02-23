76d1279334c02f64369801a544a2fa678a3acf0e
jaded-x <bayne.jaded@gmail.com>
2023-02-23 23:45:40 +1100

    updated .gitignore

--- egui_inspector_derive/target/debug/deps/unicode_ident-488d9b1a6199d7b2.d
+++
@@ -1,8 +0,0 @@
-/home/jaded/dev/egui_inspector/egui_inspector_derive/target/debug/deps/unicode_ident-488d9b1a6199d7b2.rmeta: /home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs /home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs
-
-/home/jaded/dev/egui_inspector/egui_inspector_derive/target/debug/deps/libunicode_ident-488d9b1a6199d7b2.rlib: /home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs /home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs
-
-/home/jaded/dev/egui_inspector/egui_inspector_derive/target/debug/deps/unicode_ident-488d9b1a6199d7b2.d: /home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs /home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs
-
-/home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/lib.rs:
-/home/jaded/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.6/src/tables.rs:
--- egui_inspector_derive/target/debug/libegui_inspector_derive.d
+++
@@ -1,1 +0,0 @@
-/home/jaded/dev/egui_inspector/egui_inspector_derive/target/debug/libegui_inspector_derive.so: /home/jaded/dev/egui_inspector/egui_inspector_derive/src/lib.rs
